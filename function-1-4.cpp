84ad88f63b1d537785da0c35ca5b4fafebb2d235
MAX <2638995179lyx@gmail.com>
2025-08-13 23:20:36 +0930

    practical

--- function-1-4.cpp
+++
@@ -1,9 +0,0 @@
-int sum_two_arrays(int array1[], int secondaryarray[], int n) {
-    if (n < 1) return 0;
-
-    int total = 0;
-    for (int i = 0; i < n; i++) {
-        total += array1[i] + secondaryarray[i];
-    }
-    return total;
-}
